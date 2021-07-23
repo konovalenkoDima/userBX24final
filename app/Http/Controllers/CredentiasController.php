f51e3adbd4b74a57a08a70888cac92dbb5231ee9
konovalenko <dimon200010@gmail.com>
2021-07-23 17:38:52 +0300

    Реализация обновления токена без крона

--- app/Http/Controllers/CredentiasController.php
+++ app/Http/Controllers/CredentiasController.php
@@ -45,6 +45,36 @@ class CredentiasController extends Controller
             die();
         }
 
+        if (date_diff(new \DateTime($record->created_at), new \DateTime())->format("h") >= 1)
+        {
+            $grand_type = 'refresh_token';
+            $client_id = env('B24_APPLICATION_ID');
+            $client_secret = env('B24_APPLICATION_SECRET');
+            $refresh_token = $record->refresh_id;
+            $url = 'https://oauth.bitrix.info/oauth/token/?grant_type='.$grand_type.'&client_id='.$client_id.
+                    '&client_secret='.$client_secret.'&refresh_token='.$refresh_token;
+
+            $curl = curl_init($url);
+            curl_setopt_array($curl, [
+                CURLOPT_HEADER => true,
+                CURLOPT_POST => true,
+                CURLOPT_RETURNTRANSFER => true
+            ]);
+            $result = json_decode(curl_exec($curl));
+
+            $newRecord = new Credentias;
+            $newRecord->domain = $record->domain;
+            $newRecord->lang =$record->lang;
+            $newRecord->app_sid=$record->app_sid;
+            $newRecord->auth_id=$result['access_token'];
+            $newRecord->auth_expire = $record->auth_expire ;
+            $newRecord->refresh_id = $result['refresh_token'];
+            $newRecord->member_id = $record->member_id;
+            $newRecord->save();
+
+            curl_close($curl);
+        }
+
         $obB24App = new \Bitrix24\Bitrix24(false);
         $obB24App->setApplicationSecret(env('B24_APPLICATION_SECRET'));
         $obB24App->setApplicationId(env('B24_APPLICATION_ID'));
